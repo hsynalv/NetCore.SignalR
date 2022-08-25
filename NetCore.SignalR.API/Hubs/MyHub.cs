561be900c820efa6d919e2f61d44bbc6c13b7b24
hhsynalv <hhsynalv@gmail.com>
2022-08-25 12:42:00 +0300

    OnConnectedAsync and OnDisconnectedAsync Server Hub virtual methods

--- NetCore.SignalR.API/Hubs/MyHub.cs
+++ NetCore.SignalR.API/Hubs/MyHub.cs
@@ -4,7 +4,9 @@ namespace NetCore.SignalR.API.Hubs
 {
     public class MyHub : Hub
     {
-        public static List<string> Names { get; set; } = new();
+        private static List<string> Names { get; set; } = new();
+        private static int ClientCount { get; set; } = 0;
+        
         public async Task SendName(string name)
         {
             Names.Add(name);
@@ -14,6 +16,21 @@ namespace NetCore.SignalR.API.Hubs
         public async Task GetNames()
         {
             await Clients.All.SendAsync("ReceiveNames", Names);
+        }
+
+        public async override Task OnConnectedAsync()
+        {
+            ClientCount++;
+            await Clients.All.SendAsync("ReceiveClientCount", ClientCount);
+
+            await base.OnConnectedAsync();
+        }
+
+        public async override Task OnDisconnectedAsync(Exception? exception)
+        {
+            ClientCount--;
+            await Clients.All.SendAsync("ReceiveClientCount", ClientCount);
+            await base.OnDisconnectedAsync(exception);
         }
     }
 }
